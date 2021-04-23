c6bb0ed0dc3f1db9c49fbb5297f52446396e9f85
fernandossmm <UO266754@uniovi.es>
2021-04-23 13:39:58 +0200

    Fix try #2

--- scripts.js
+++ scripts.js
@@ -97,19 +97,9 @@ function submitContact() {
     user: user_uuid()
   };
   
-  var url="https://prometheus-tagging.herokuapp.com/contact";
-
-  $.post({
-    url: url,
-    data: JSON.stringify(data),
-    contentType: "application/json; charset=utf-8",
-    success: function (resp) {
-        
-    },
-    error: function(r, e) {
-        console.log('Error sending tagging data: ' + e);
-    }  
-  }, "json");
+  dataLayer.push({'event': 'submit-contact', 'data': data});
+  
+  window.location.href = "#";
 }
 
 // Tickets
